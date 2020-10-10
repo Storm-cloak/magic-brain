ad048d469a5d66b80736f6437ae021067a988592
Storm-cloak <raghamoghlanov@gmail.com>
2020-10-11 03:21:40 +0400

    a little bit of salt

--- face-detection-front/src/components/Register/register.component.jsx
+++ face-detection-front/src/components/Register/register.component.jsx
@@ -30,8 +30,8 @@ const RegisterForm = ({ onRouteChange, loadUser }) => {
       .then((user) => {
         if (user.id) {
           console.log(user);
-          loadUser(user);
           onRouteChange("home");
+          loadUser(user);
         }
       });
   };
@@ -39,63 +39,61 @@ const RegisterForm = ({ onRouteChange, loadUser }) => {
   return (
     <>
       <article className="br3 ba  b--black-10  w-100 w-50-m w-25-l mw6 shadow-5 center">
-        <form>
-          <main className="pa4 black-80">
-            <div className="measure center">
-              <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                <legend className="f1 fw6 ph0 mh0">Register</legend>
-                <div className="mt3">
-                  <label className="db fw6 lh-copy f6" htmlFor="name">
-                    Name
-                  </label>
-                  <input
-                    className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                    type="text"
-                    name="name"
-                    id="name"
-                    onChange={onNameChange}
-                    required
-                  />
-                </div>
-                <div className="mt3">
-                  <label className="db fw6 lh-copy f6" htmlFor="email-address">
-                    Email
-                  </label>
-                  <input
-                    className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                    type="email"
-                    name="email-address"
-                    id="email-address"
-                    required
-                    onChange={onEmailChange}
-                  />
-                </div>
-                <div className="mv3">
-                  <label className="db fw6 lh-copy f6" htmlFor="password">
-                    Password
-                  </label>
-                  <input
-                    className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                    type="password"
-                    name="password"
-                    id="password"
-                    required
-                    onChange={onPasswordChange}
-                  />
-                </div>
-              </fieldset>
-              <div className="">
+        <main className="pa4 black-80">
+          <div className="measure center">
+            <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+              <legend className="f1 fw6 ph0 mh0">Register</legend>
+              <div className="mt3">
+                <label className="db fw6 lh-copy f6" htmlFor="name">
+                  Name
+                </label>
                 <input
-                  className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
-                  type="submit"
-                  value="Register"
-                  onClick={onSubmitRegister}
+                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  type="text"
+                  name="name"
+                  id="name"
+                  onChange={onNameChange}
+                  required
                 />
               </div>
-              <div className="lh-copy mt3"></div>
+              <div className="mt3">
+                <label className="db fw6 lh-copy f6" htmlFor="email-address">
+                  Email
+                </label>
+                <input
+                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  type="email"
+                  name="email-address"
+                  id="email-address"
+                  required
+                  onChange={onEmailChange}
+                />
+              </div>
+              <div className="mv3">
+                <label className="db fw6 lh-copy f6" htmlFor="password">
+                  Password
+                </label>
+                <input
+                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  type="password"
+                  name="password"
+                  id="password"
+                  required
+                  onChange={onPasswordChange}
+                />
+              </div>
+            </fieldset>
+            <div className="">
+              <input
+                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+                type="submit"
+                value="Register"
+                onClick={onSubmitRegister}
+              />
             </div>
-          </main>
-        </form>
+            <div className="lh-copy mt3"></div>
+          </div>
+        </main>
       </article>
     </>
   );
--- face-detection-front/src/components/SignIn/Signin.component.jsx
+++ face-detection-front/src/components/SignIn/Signin.component.jsx
@@ -67,9 +67,9 @@ const SignInForm = ({ onRouteChange, loadUser }) => {
             </fieldset>
             <div className="">
               <input
-                className="b ph3 w-25 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
                 type="submit"
-                value="Sign in"
+                value=" Sign in "
                 onClick={onSubmitSignIn}
               />
             </div>
