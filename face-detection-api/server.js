8015cd3ddff469ab29724d2c35575f3c68150129
Storm-cloak <raghamoghlanov@gmail.com>
2020-10-10 22:02:56 +0400

    restructured folders

---
+++ face-detection-api/server.js
@@ -0,0 +1,97 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+const database = {
+  users: [
+    {
+      id: "1",
+      name: "Emin",
+      password: "kek42",
+      email: "emin@gmail.com",
+      entries: 0,
+      joined: new Date(),
+    },
+    {
+      id: "2",
+      name: "Rustam",
+      password: "lol42",
+      email: "rustam@gmail.com",
+      entries: 0,
+      joined: new Date(),
+    },
+  ],
+};
+
+app.get("/", (req, res) => {
+  res.send(database.users);
+});
+
+app.post("/signin", (req, res) => {
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    res.json(database.users[0]);
+  } else {
+    res.status(400).json("error logging in");
+  }
+});
+
+app.post("/register", (req, res) => {
+  const { email, name, password } = req.body;
+  database.users.push({
+    id: Math.random() * 1000,
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date(),
+  });
+  res.json(database.users[database.users.length - 1]);
+});
+
+app.get("/profile/:id", (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  });
+  if (!found) {
+    res.status(400).json("not found");
+  }
+});
+
+app.put("/image", (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  });
+  if (!found) {
+    res.status(400).json("not found");
+  }
+});
+
+app.listen(3001, () => {
+  console.log("app is running");
+});
+
+/*
+/ --> res = this is working
+/signin --> POST (post some request, info) = success / fail
+/register --> POST (we want to add data to database) = return user object
+/profile/:userid --> GET (get user info) = user
+/image --> PUT (update) --> user
+
+*/
